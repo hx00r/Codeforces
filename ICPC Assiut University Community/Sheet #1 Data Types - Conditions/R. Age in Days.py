a51bfe470d4f35efaebfb72061788d7202165500
Hx00r <33162573+hx00r@users.noreply.github.com>
2023-09-03 18:59:15 +0300

    problem is solved in one line

--- ICPC Assiut University Community/Sheet #1 Data Types - Conditions/R. Age in Days.py
+++ ICPC Assiut University Community/Sheet #1 Data Types - Conditions/R. Age in Days.py
@@ -1,8 +1,1 @@
-age = int(input(""))
-years = age // 365
-remaining_days = age % 365
-months = remaining_days // 30
-days = remaining_days % 30
-print(years, 'years')
-print(months, 'months')
-print(days, 'days')
+a=int(input());print(f"{a//365} years\n{a%365//30} months\n{a%365%30} days")
